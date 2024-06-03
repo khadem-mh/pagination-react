513c3fdee8e0d201d4d1a563fcdd12907589cf04
khadem-mh <khadem13359@gmail.com>
2024-06-02 21:17:44 -0700

    remove folder

--- Pagination/PaginationLi.jsx
+++
@@ -1,7 +0,0 @@
-export default function PaginationLi({item, pageActive, colorActive, color, bgColor, bgColorActive, clickHandlerPagination}) {
-    return (
-        <li key={item} className={`pagination-item ${pageActive === item + 1 ? 'pagination-item-disable' : ''}`} style={{ color: pageActive === item + 1 ? colorActive : color, backgroundColor: pageActive === item + 1 ? bgColorActive : bgColor }} onClick={() => clickHandlerPagination(item + 1)}>
-            <p>{item + 1}</p>
-        </li>
-    )
-}
